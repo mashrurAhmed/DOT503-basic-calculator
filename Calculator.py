e59590f4f72f67d8d7f140ecb78ac14df9af86a1
Mashrur Ahmed <mashrur.7@gmail.com>
2022-04-15 03:21:11 +1000

    Added exception for ValueError

--- Calculator.py
+++ Calculator.py
@@ -21,22 +21,23 @@ class Calculator:
         return self.result
 
     def calculate(self, string1):
-        input_list = string1.split(' ')
-        if(input_list[0]) == 'r':
-            num1 = self.result
-        else:
-            num1 = float(input_list[0])
-        num2 = float(input_list[2])
-        if(input_list[1]) == '+':
-            self.result = self.add(num1, num2)
-        elif(input_list[1]) == '-':
-            self.result = self.substract(num1, num2)
-        elif(input_list[1]) == '*':
-            self.result = self.multiply(num1, num2)
-        elif(input_list[1]) == '/':
-            self.result = self.divide(num1, num2)
-        else:
-            print("Invalid operator")
+        try:
+            input_list = string1.split(' ')
+            if(input_list[0]) == 'r':
+                num1 = self.result
+            else:
+                num1 = float(input_list[0])
+            num2 = float(input_list[2])
+            if(input_list[1]) == '+':
+                self.result = self.add(num1, num2)
+            elif(input_list[1]) == '-':
+                self.result = self.substract(num1, num2)
+            elif(input_list[1]) == '*':
+                self.result = self.multiply(num1, num2)
+            elif(input_list[1]) == '/':
+                self.result = self.divide(num1, num2)
+        except ValueError:
+            print("Please enter numbers or 'r'. Refer to README for format.")
         return self.result
 
     def getresult(self):
